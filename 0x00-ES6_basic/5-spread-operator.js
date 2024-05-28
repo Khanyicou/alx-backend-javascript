f1dc6ed0bf248a0ffc85a28d1050c28918757a52
Khanyicou <preciouskhanyisa1@gmail.com>
2024-05-27 20:21:59 -0700

    ou

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,5 @@
+// Export a function named concatArrays as the default export
 export default function concatArrays(array1, array2, string) {
-    return [...array1, ...array2, ...string];
-  }
+  // Return a new array created by concatenating array1, array2, and the characters of the string
+  return [...array1, ...array2, ...string];
+}
